3e31abdd99bff41d047f79ba8046de6663f1df9c
elyonjohn <elyonjohn60@gmail.com>
2024-04-02 13:15:21 +0100

    Include and Namespace In this commit i created the header file library that will help us work with input and output objects Such as the cout and cin, Then I created a function, Then created the 3 cout objects, And a created the cin object for the user to enter data.

---
+++ one-array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int students;
+	cout << "Welcome to student Score Analysis:";
+	cout << "Friendly Student Calculator";
+	cout << "How many students are in your class: ";
+	cin >> students;
+}
